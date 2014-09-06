c08004af24b5d179e4d3bf1803d16163de96ff64
bryandbor <bryandbor@gmail.com>
2014-09-06 14:13:30 -0700

    Initial setup
    HTML page now has libraries from JQuery and React.js.  Also the folder
    structure has been made that will house the Javascript (js), CSS, and
    PHP files

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>Go Social</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <script src="./build/react.js"></script>
+        <script src="./build/JSXTransformer.js"></script>
+        <link href="./css/index.css" rel="stylesheet">
+        <link href="./css/bootstrap.min.css" rel="stylesheet">
+    </head>
+    <body>
+    </body>
+</html>
