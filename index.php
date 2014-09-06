f93aed3338a1754e628652a3cb3ffab217403b6f
bryandbor <bryandbor@gmail.com>
2014-09-06 14:16:40 -0700

    Better HTML page organization

--- index.php
+++ index.php
@@ -2,11 +2,12 @@
     <head>
         <title>Go Social</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <script src="./build/react.js"></script>
-        <script src="./build/JSXTransformer.js"></script>
         <link href="./css/index.css" rel="stylesheet">
         <link href="./css/bootstrap.min.css" rel="stylesheet">
+        <script src="./build/react.js"></script>
+        <script src="./build/JSXTransformer.js"></script>
     </head>
     <body>
+    	<script src="./js/index.js" type="text/jsx"></script>
     </body>
 </html>
